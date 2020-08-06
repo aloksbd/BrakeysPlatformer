9c54a9d34773ddcda2dfceece092b164870ad33a
aloksbd <30898579+aloksbd@users.noreply.github.com>
2020-08-06 17:30:21 +0545

    dash + new autotile

--- scripts/player.gd
+++ scripts/player.gd
@@ -8,21 +8,36 @@ const JUMP = -200
 var motion = Vector2()
 
 var bullet = null
+var dash = 0
 
 func _physics_process(delta):
+	if dash == 1:
+		motion = move_and_slide(motion,UP)
+		return
 	motion.y += GRAVITY
 	if Input.is_action_pressed("ui_right"):
+		$Sprite.flip_h = true
 		motion.x = min(motion.x + ACCELERATION, SPEED)
 	elif Input.is_action_pressed("ui_left"):
+		$Sprite.flip_h = false
 		motion.x = max(motion.x - ACCELERATION, -SPEED)
 	else:
 		motion.x = 0
 		
 	if is_on_floor():
+		dash = 0
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = JUMP	
 		motion.x = lerp(motion.x,0,0.2)
 	else:
+		if Input.is_action_just_pressed("dash") && dash == 0:
+			dash = 1
+			motion.x = -500
+			if $Sprite.flip_h:
+				motion.x = 500
+			motion.y = 0
+			yield(get_tree().create_timer(0.2),"timeout")
+			dash = 2
 		motion.x = lerp(motion.x,0,0.05)
 	motion = move_and_slide(motion,UP)
 	
